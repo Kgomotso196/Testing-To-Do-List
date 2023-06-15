ef37b3584bcd56bed526cd5a5de45221c8e51ec4
Kgomotso Nacane <kgomotsonacane@gmail.com>
2023-06-15 12:25:47 +0200

    Fix linter errors

---
+++ mockStorage.js
@@ -0,0 +1,31 @@
+class MockStorage {
+  constructor() {
+    this.store = {};
+  }
+
+  getItem(key) {
+    return this.store[key] || null;
+  }
+
+  setItem(key, value) {
+    this.store[key] = value.toString();
+  }
+
+  removeItem(key) {
+    delete this.store[key];
+  }
+
+  clear() {
+    this.store = {};
+  }
+
+  get length() {
+    return Object.keys(this.store).length;
+  }
+
+  key(index) {
+    return Object.keys(this.store)[index] || null;
+  }
+}
+
+exports.MockStorage = MockStorage;
---
+++ tests/functionality.test.js
@@ -0,0 +1,49 @@
+import Functionality from '../src/modules/functionality.js';
+
+describe('newTask', () => {
+  beforeEach(() => {
+    // Clear localStorage before each test
+    localStorage.clear();
+  });
+
+  test('newTask should add a new task to the array, then verify', () => {
+    // Set up the initial test environment with the item properties to be added to existing array
+    document.body.innerHTML = `
+            <div id="todo-holder"></div>
+            <input type="text" id="text-section" value="for testing" />
+            <form id="text-form">
+                <input type="submit" id="submit-button" />
+            </form>
+        `;
+
+    // Set up the existing tasks
+    const tasks = [{ index: 1, description: 'existing task', completed: false }];
+    localStorage.setItem('data', JSON.stringify(tasks));
+
+    // Call the newTask function
+    Functionality.newTask();
+
+    // Verify the task is added to the array
+    const updatedTasks = Functionality.getAllTasks();
+    expect(updatedTasks).toHaveLength(2);
+    expect(updatedTasks[0].description).toBe('existing task');
+    expect(updatedTasks[1].description).toBe('for testing');
+    expect(updatedTasks[1].completed).toBe(false);
+    expect(updatedTasks[1].index).toBe(2);
+
+    // Verify the task is saved to localStorage
+    expect(localStorage.getItem('data')).not.toBeNull();
+    const storedTasks = JSON.parse(localStorage.getItem('data'));
+    expect(storedTasks).toHaveLength(2);
+    expect(storedTasks[0].description).toBe('existing task');
+    expect(storedTasks[0].completed).toBe(false);
+    expect(storedTasks[0].index).toBe(1);
+    expect(storedTasks[1].description).toBe('for testing');
+    expect(storedTasks[1].completed).toBe(false);
+    expect(storedTasks[1].index).toBe(2);
+
+    // Verify the task is displayed
+    const taskElement = document.querySelector('#todo-holder li');
+    expect(taskElement).not.toBeNull();
+  });
+});
